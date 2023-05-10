795774d5b088cc771989c17125eb4eee6bb3a146
AnastasiaSvyat <64147723+AnastasiaSvyat@users.noreply.github.com>
2023-05-10 21:27:39 +0200

    created components

--- usersActions/src/app/app.module.ts
+++ usersActions/src/app/app.module.ts
@@ -4,10 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UserDetailsComponent } from './components/users-page/user-details/user-details.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { UsersPageComponent } from './components/users-page/users-page.component';
+import { RepositoriesPageComponent } from './components/repositories-page/repositories-page.component';
+import { FavoritesPageComponent } from './components/favorites-page/favorites-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    UsersPageComponent,
+    RepositoriesPageComponent,
+    FavoritesPageComponent,
+    UserDetailsComponent
   ],
   imports: [
     BrowserModule,
---
+++ usersActions/src/app/components/repositories-page/repositories-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-repositories-page',
+  templateUrl: './repositories-page.component.html',
+  styleUrls: ['./repositories-page.component.scss']
+})
+export class RepositoriesPageComponent {
+
+}
